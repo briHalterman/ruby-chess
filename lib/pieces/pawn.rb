c0fd3597a06bf15460bf4af998c312b746719505
Brittany Halterman <halterma@gmail.com>
2025-05-12 19:41:43 -0400

    Define Pawn#symbol to return correct Unicode character

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -4,5 +4,6 @@ require_relative 'piece'
 class Pawn < Piece
   def symbol
     return '♙' if color == :white
+    return '♟' if color == :black
   end
 end
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -4,10 +4,15 @@ require_relative '../../lib/pieces/pawn'
 
 RSpec.describe Pawn do
   let(:white_pawn) { Pawn.new(:white, [1, 0]) }
+  let(:black_pawn) { Pawn.new(:black, [6, 0]) }
 
   describe '#symbol' do
     it 'returns ♙ for white pawns' do
       expect(white_pawn.symbol).to eq('♙')
+    end
+
+    it 'returns ♟ for black pawns' do
+      expect(black_pawn.symbol).to eq('♟')
     end
   end
 end
