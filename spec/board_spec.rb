fb5f23f9c881e205843b23b258d19feb8c924046
Brittany Halterman <halterma@gmail.com>
2025-05-12 14:16:18 -0400

    Refactor: Replace repeated Board.new with let(:board)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 require_relative '../lib/board'
 
 RSpec.describe Board do
+  let(:board) { Board.new }
+
   describe '#initialize' do
     it 'creates an 8x8 grid' do
-      board = Board.new
       expect(board.grid.size).to eq(8)
       expect(board.grid.all? { |row| row.size == 8 }).to be true
     end
@@ -13,7 +14,6 @@ RSpec.describe Board do
 
   describe '#place_piece' do
     it 'places a piece at the given position on the grid' do
-      board = Board.new
       mock_piece = double('Piece')
       board.place_piece(mock_piece, [3, 4])
 
@@ -23,7 +23,6 @@ RSpec.describe Board do
 
   describe '#piece_at' do
     it 'returns the piece at the given position' do
-      board = Board.new
       mock_piece = double('Piece')
       board.place_piece(mock_piece, [1, 1])
 
