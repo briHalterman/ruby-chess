e5580f88ca8a03dae1eef97ed1163fb33cfbd8c4
Brittany Halterman <halterma@gmail.com>
2025-05-12 14:59:46 -0400

    Add example placeholder pieces to the board

--- lib/board.rb
+++ lib/board.rb
@@ -25,6 +25,9 @@ class Board
   end
 
   def place_starting_pieces
-    
+    mock_piece = :white_rook  # placeholder implementation
+    place_piece(mock_piece, [0, 0])
+    place_piece(mock_piece, [1, 0])
+    place_piece(mock_piece, [1, 4])
   end
 end
