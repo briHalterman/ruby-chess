126608507434d35838249502acbb2487c2fe7913
Brittany Halterman <halterma@gmail.com>
2025-05-12 19:13:43 -0400

    Add base Piece class with #symbol raising NotImplementedError

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,4 +1,13 @@
 # lib/pieces/piece.rb
 class Piece
-  
+  attr_reader :color, :position
+
+  def initialize(color, position)
+    @color = color
+    @position = position
+  end
+
+  def symbol
+    raise NotImplementedError, 'Each subclass must define a `symbol` method.'
+  end
 end
