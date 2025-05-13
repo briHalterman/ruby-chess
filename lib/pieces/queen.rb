c3ab65ae59bccfd777fd00e59631092b890c261a
Brittany Halterman <halterma@gmail.com>
2025-05-13 17:40:29 -0400

    Define valid_move? for queens

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -5,4 +5,15 @@ class Queen < Piece
   def symbol
     color == :white ? '♕' : '♛'
   end
+
+  def valid_move?(from_position, to_position, board)
+    row_delta = (to_position[0] - from_position[0]).abs
+    column_delta = (to_position[1] - from_position[1]).abs
+
+    return true if row_delta == 0 && column_delta > 0
+    return true if column_delta == 0 && row_delta > 0
+    return true if row_delta == column_delta
+
+    false
+  end
 end
--- spec/pieces/queen_spec.rb
+++ spec/pieces/queen_spec.rb
@@ -5,6 +5,8 @@ require_relative '../../lib/pieces/queen'
 RSpec.describe Queen do
   let(:white_queen) { Queen.new(:white, [0, 4]) }
   let(:black_queen) { Queen.new(:black, [7, 4]) }
+  let(:queen) { Queen.new(:white, [0, 3])}
+  let(:board) { double("Board")}
 
   describe '#symbol' do
     it 'returns ♕ for white queens' do
@@ -13,6 +15,24 @@ RSpec.describe Queen do
 
     it 'returns ♛ for black queens' do
       expect(black_queen.symbol).to eq('♛')
+    end
+  end
+
+  describe '#valid_move?' do
+    it 'returns true for a vertical move' do
+      expect(queen.valid_move?([0, 3], [4, 3], board)).to be true
+    end
+
+    it 'returns true for a horizontal move' do
+      expect(queen.valid_move?([0, 3], [0, 6], board)).to be true
+    end
+
+    it 'returns true for a diagonal move' do
+      expect(queen.valid_move?([0, 3], [3, 6], board)).to be true
+    end
+
+    it 'returns false for an invalid L-shaped move' do
+      expect(queen.valid_move?([0, 3], [1, 5], board)).to be false
     end
   end
 end
