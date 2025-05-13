fb7a653907c513c00511df7c5bc591f61195e66b
Brittany Halterman <halterma@gmail.com>
2025-05-13 17:21:47 -0400

    Define valid_move? for knights

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -5,4 +5,11 @@ class Knight < Piece
   def symbol
     color == :white ? '♘' : '♞'
   end
+
+  def valid_move?(from_position, to_position, board)
+    row_delta = (to_position[1] - from_position[1]).abs
+    column_delta = (to_position[0] - from_position[0]).abs
+
+    (row_delta == 2 && column_delta == 1) || (row_delta == 1 && column_delta == 2)
+  end
 end
--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -5,6 +5,7 @@ require_relative '../../lib/pieces/knight'
 RSpec.describe Knight do
   let(:white_knight) { Knight.new(:white, [0, 1]) }
   let(:black_knight) { Knight.new(:black, [7, 1]) }
+  let(:board) { double("Board")}
 
   describe '#symbol' do
     it 'returns ♘ for white knights' do
@@ -13,6 +14,26 @@ RSpec.describe Knight do
 
     it 'returns ♞ for black knights' do
       expect(black_knight.symbol).to eq('♞')
+    end
+  end
+
+  describe '#valid_move?' do
+    let(:knight) { Knight.new(:white, [7, 1]) }
+
+    it 'returns true for a valid L-shaped move (2 up, 1 right)' do
+      expect(knight.valid_move?([7, 1], [5, 2], board)).to be true
+    end
+
+    it 'returns true for a valid L-shaped move (2 left 1 up)' do
+      expect(knight.valid_move?([7, 1], [6, 3], board)).to be true
+    end
+
+    it 'returns false for a straight move' do
+      expect(knight.valid_move?([7, 1], [5, 1], board)).to be false
+    end
+
+    it 'returns false for a diagonal move' do
+      expect(knight.valid_move?([7, 1], [6, 2], board)).to be false
     end
   end
 end
