0cebefeb6e292980c5c8cd1405e8d04569506d07
Brittany Halterman <halterma@gmail.com>
2025-05-13 17:31:26 -0400

    Define valid_move? for bishops

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -5,4 +5,11 @@ class Bishop < Piece
   def symbol
     color == :white ? '♗' : '♝'
   end
+
+  def valid_move?(from_position, to_position, board)
+    row_delta = (to_position[0] - from_position[0]).abs
+    column_delta = (to_position[1] - from_position[1]).abs
+
+    row_delta == column_delta
+  end
 end
--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -5,6 +5,8 @@ require_relative '../../lib/pieces/bishop'
 RSpec.describe Bishop do
   let(:white_bishop) { Bishop.new(:white, [0, 2]) }
   let(:black_bishop) { Bishop.new(:black, [7, 2]) }
+  let(:bishop) { Bishop.new(:white, [0, 2]) }
+  let(:board) { double("Board") }
 
   describe '#symbol' do
     it 'returns ♗ for white bishops' do
@@ -13,6 +15,20 @@ RSpec.describe Bishop do
 
     it 'returns ♝ for black bishops' do
       expect(black_bishop.symbol).to eq('♝')
+    end
+  end
+
+  describe '#valid_move?' do
+    it 'returns true for a valid diagonal move' do
+      expect(bishop.valid_move?([0, 2], [3, 5], board)).to be true
+    end
+
+    it 'returns false for a horizontal move' do
+      expect(bishop.valid_move?([0, 2], [0, 5], board)).to be false
+    end
+
+    it 'returns false for a vertical move' do
+      expect(bishop.valid_move?([0, 2], [3, 2], board)).to be false
     end
   end
 end
