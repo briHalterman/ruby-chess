9e1f0cdfd848399c6bd71e7e964d13660b9b5bc7
Brittany Halterman <halterma@gmail.com>
2025-05-13 15:30:00 -0400

    Define valid_move? for pawns

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -5,4 +5,21 @@ class Pawn < Piece
   def symbol
     color == :white ? '♙' : '♟'
   end
+
+  def valid_move?(from_position, to_position, board)
+    # Only vertical moves
+    row_delta = to_position[0] - from_position[0]
+    column_delta = (to_position[1] - from_position[1]).abs
+
+    # Pawns don't move sideways
+    return false unless column_delta == 0
+
+    if color == :white
+      return true if row_delta == 1 # One square forward
+      return true if from_position[0] == 1 && row_delta == 2 # Opening two-square move
+    else
+      return true if row_delta == -1
+      return true if from_position[0] == 6 && row_delta == 2
+    end
+  end
 end
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -5,6 +5,7 @@ require_relative '../../lib/pieces/pawn'
 RSpec.describe Pawn do
   let(:white_pawn) { Pawn.new(:white, [1, 0]) }
   let(:black_pawn) { Pawn.new(:black, [6, 0]) }
+  let(:board) { double("Board") }
 
   describe '#symbol' do
     it 'returns ♙ for white pawns' do
@@ -13,6 +14,22 @@ RSpec.describe Pawn do
 
     it 'returns ♟ for black pawns' do
       expect(black_pawn.symbol).to eq('♟')
+    end
+  end
+
+  context 'white pawn' do
+    let(:pawn) { Pawn.new(:white, [1, 4]) }
+
+    it 'can move one space forward' do
+      expect(pawn.valid_move?([1, 4], [2, 4], board)).to be true
+    end
+
+    it 'can move two space from starting position' do
+      expect(pawn.valid_move?([1, 4], [3, 4], board)).to be true
+    end
+
+    it 'cannot move sideways' do
+      expect(pawn.valid_move?([1, 4], [1, 5], board)).to be false
     end
   end
 end
