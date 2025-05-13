e6a63984e102c91fad2010330ffa1e2477e16db7
Brittany Halterman <halterma@gmail.com>
2025-05-13 15:42:41 -0400

    Define valid_move? for rooks

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -5,4 +5,14 @@ class Rook < Piece
   def symbol
     color == :white ? '♖' : '♜'
   end
+
+  def valid_move?(from_position, to_position, board)
+    row_delta = to_position[0] - from_position[0]
+    column_delta = to_position[1] - from_position[1]
+
+    return false if row_delta != 0 && column_delta != 0
+    return false if row_delta == 0 && column_delta == 0
+
+    true
+  end
 end
--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -5,6 +5,7 @@ require_relative '../../lib/pieces/rook'
 RSpec.describe Rook do
   let(:white_rook) { Rook.new(:white, [0, 0]) }
   let(:black_rook) { Rook.new(:black, [7, 0]) }
+  let(:board) { double("Board") }
 
   describe '#symbol' do
     it 'returns ♖ for white rooks' do
@@ -13,6 +14,22 @@ RSpec.describe Rook do
 
     it 'returns ♜ for black rooks' do
       expect(black_rook.symbol).to eq('♜')
+    end
+  end
+
+  describe '#valid_move?' do
+    let(:rook) { Rook.new(:white, [0, 0]) }
+
+    it 'returns true for a vertical move' do
+      expect(rook.valid_move?([0, 0], [5, 0], board)).to be true
+    end
+
+    it 'returns false for a diagonal move' do
+      expect(rook.valid_move?([0, 0], [3, 3], board)).to be false
+    end
+
+    it 'returns false if move does not change row or column' do
+      expect(rook.valid_move?([0, 0], [0, 0], board)).to be false
     end
   end
 end
