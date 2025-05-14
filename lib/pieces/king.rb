44dfd33d622d0309d00c90970fcca3959851ac51
Brittany Halterman <halterma@gmail.com>
2025-05-14 08:53:27 -0400

    Rafactor king.rb for clarity

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,4 +1,4 @@
-# lib/pieces/pawn.rb
+# lib/pieces/king.rb
 require_relative 'piece'
 
 class King < Piece
@@ -7,9 +7,11 @@ class King < Piece
   end
 
   def valid_move?(from_position, to_position, board)
+    return false if from_position == to_position
+
     row_delta = (to_position[0] - from_position[0]).abs
     column_delta = (to_position[1] - from_position[1]).abs
 
-    row_delta <= 1 && column_delta <= 1 && (row_delta + column_delta != 0)
+    row_delta <= 1 && column_delta <= 1
   end
 end
